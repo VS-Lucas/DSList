3cc870702fcdec2c3a607cb11a699cb556f3017d
VS-Lucas <ufpelucasv@gmail.com>
2024-11-20 18:40:14 -0300

    refactor: endpoints' response type

--- src/main/java/com/lucasprojetos/dslist/controller/GameController.java
+++ src/main/java/com/lucasprojetos/dslist/controller/GameController.java
@@ -3,6 +3,8 @@ package com.lucasprojetos.dslist.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.lucasprojetos.dslist.dto.GameDTO;
 import com.lucasprojetos.dslist.dto.GameMinDTO;
+import com.lucasprojetos.dslist.response.BuildResponse;
+import com.lucasprojetos.dslist.response.Response;
 import com.lucasprojetos.dslist.services.GameService;
 
 @RestController
@@ -19,16 +23,20 @@ public class GameController {
     @Autowired
     private GameService gameService;
 
+    @Autowired
+    private BuildResponse buildResponse;
+
     @GetMapping(value = "/{id}")
-    public GameDTO findById(@PathVariable Long id) {
-        GameDTO game = gameService.findById(id);
-        return game;
+    public ResponseEntity<Response<GameDTO>> findById(@PathVariable Long id) {
+        GameDTO gameDTO = gameService.findById(id);
+
+        return buildResponse.build("success", "Game found", gameDTO, HttpStatus.OK);
     }
 
     @GetMapping
-    public List<GameMinDTO> findAll() {
+    public ResponseEntity<Response<List<GameMinDTO>>> findAll() {
         List<GameMinDTO> games = gameService.findAll();
-        return games;
+        
+        return buildResponse.build("success", "Games found", games, HttpStatus.OK);
     }
 }
- 
--- src/main/java/com/lucasprojetos/dslist/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/lucasprojetos/dslist/exceptions/GlobalExceptionHandler.java
@@ -1,22 +1,28 @@
 package com.lucasprojetos.dslist.exceptions;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.lucasprojetos.dslist.response.BuildResponse;
+import com.lucasprojetos.dslist.response.Response;
+
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @Autowired
+    private BuildResponse buildResponse;
+
     @ExceptionHandler({GameNotFoundException.class})
-    public ResponseEntity<Object> handleGameNotFoundException(GameNotFoundException exception) {
-        return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
-                .body(exception.getMessage());
+    public ResponseEntity<Response<String>> handleGameNotFoundException(GameNotFoundException exception) {
+        return buildResponse.build("error", exception.getMessage(), null, HttpStatus.NOT_FOUND);        
     }
 
     @ExceptionHandler({RuntimeException.class})
-    public ResponseEntity<Object> handleRuntimeException(RuntimeException exception) {
-        return ResponseEntity
-                .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(exception.getMessage());
+    public ResponseEntity<Response<String>> handleRuntimeException(RuntimeException exception) {
+        return buildResponse.build("error", "Internal Server Error", null, HttpStatus.INTERNAL_SERVER_ERROR);  
     }
 }
