7c08bf14729b1b4d0bd2fffd5bec636bf9910409
VS-Lucas <ufpelucasv@gmail.com>
2024-11-20 17:21:35 -0300

    feat: globa exception handler

---
+++ src/main/java/com/lucasprojetos/dslist/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.lucasprojetos.dslist.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler({GameNotFoundException.class})
+    public ResponseEntity<Object> handleGameNotFoundException(GameNotFoundException exception) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(exception.getMessage());
+    }
+
+    @ExceptionHandler({RuntimeException.class})
+    public ResponseEntity<Object> handleRuntimeException(RuntimeException exception) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(exception.getMessage());
+    }
+}
