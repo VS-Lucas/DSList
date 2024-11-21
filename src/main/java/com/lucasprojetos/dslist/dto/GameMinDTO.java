616696c48b94f91eb0472b40e4706ebe333638e6
VS-Lucas <ufpelucasv@gmail.com>
2024-11-21 00:37:18 -0300

    feat: homolog postgres

--- src/main/java/com/lucasprojetos/dslist/dto/GameMinDTO.java
+++ src/main/java/com/lucasprojetos/dslist/dto/GameMinDTO.java
@@ -5,31 +5,31 @@ import com.lucasprojetos.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
 
-	private Long id;
-	private String title;
-	private Integer year;
-	private String imgUrl;
-	private String shortDescription;
+    private Long id;
+    private String title;
+    private Integer year;
+    private String imgUrl;
+    private String shortDescription;
 
     public GameMinDTO() {
     }
 
-	public GameMinDTO(Game entity) {
-		id = entity.getId();
-		title = entity.getTitle();
-		year = entity.getYear();
-		imgUrl = entity.getImgUrl();
-		shortDescription = entity.getShortDescription();
-	}
+    public GameMinDTO(Game entity) {
+        id = entity.getId();
+        title = entity.getTitle();
+        year = entity.getYear();
+        imgUrl = entity.getImgUrl();
+        shortDescription = entity.getShortDescription();
+    }
 
     public GameMinDTO(GameMinProjection projection) {
-		id = projection.getId();
-		title = projection.getTitle();
-		year = projection.getYear();
-		imgUrl = projection.getImgUrl();
-		shortDescription = projection.getShortDescription();
-	}
-	
+        id = projection.getId();
+        title = projection.getTitle();
+        year = projection.getGameYear();
+        imgUrl = projection.getImgUrl();
+        shortDescription = projection.getShortDescription();
+    }
+
     public Long getId() {
         return id;
     }
